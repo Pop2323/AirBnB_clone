054c13d74e04e478d02e8bd00e8dcfd00e092e81
Pop2323 <omar4500455@gmail.com>
2023-11-17 07:56:29 -0800

    AirBnb Clone

--- models/base_model.py
+++ models/base_model.py
@@ -37,7 +37,7 @@ class BaseModel:
 
     def __str__(self):
         """Define str"""
-        return("[{}] ({}) {}".format(
+        return ("[{}] ({}) {}".format(
             self.__class__.__name__,
             self.id, self.__dict__))
 
